1d4b383057dfc83b5f0b6ce73b03f522f40962d2
ci0cc0lat0 <anthonymciocco@gmail.com>
2024-08-01 21:22:34 -0500

    Allowed no note content post

--- src/app/components/Note.js
+++ src/app/components/Note.js
@@ -3,6 +3,10 @@ import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
 export default function Note({ data }){
     const note = data.Notes;
+    if(note == null){
+        return
+    }
+    console.log(note)
     return(
         <BlocksRenderer content={note}/>
     )
