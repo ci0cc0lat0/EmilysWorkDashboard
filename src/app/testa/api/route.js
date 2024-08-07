ef791f3a7a254c5add21e7317e3eb5be50693f52
ci0cc0lat0 <anthonymciocco@gmail.com>
2024-08-07 00:04:26 -0500

    No testa

--- src/app/testa/api/route.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res){
-    const {sort} = req.query
-    let query = `/docx-tests?sort=createdAt:${sort}`
-    
-    const response = await fetch(`http://${process.env.API_IP}:1339/api/${query}`)
-    const data = await response.json();
-
-    res.status(200).json(data);
-}
