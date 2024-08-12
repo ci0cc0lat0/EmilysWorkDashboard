6e36958ff069ddf2eb3c3849aa96619f40d394ca
ci0cc0lat0 <anthonymciocco@gmail.com>
2024-08-12 15:12:05 -0500

    Table for docs page

--- src/app/docs/Docs_render.js
+++ src/app/docs/Docs_render.js
@@ -14,23 +14,82 @@ export default function Docs_render({sort_state}){
     fetch_data()
   },[sort_state])
 
- 
+   const columns = [
+    {
+      key:"Title",
+      label:"Title"
+    },
+    {
+      key:"Document_Type",
+      label:"Doc Type"
+    },
+    {
+      key:"Market",
+      label:"Market"
+    },
+    {
+      key:"Group",
+      label:"Group"
+    },
+    {
+      key:"Meeting",
+      label:"Meeting"
+    },
+    {
+      key:"Meeting_Date",
+      label:"Meeting Date"
+    }
+  ]
+  
+  
   return (
     <>
+      <table className={s.table_a}>
+        <tbody className={s.table_tbody}>
+        <tr >
+        {columns.map((column)=>
+          <th className="table_header_b" key={column.key}>{column.label}</th>)}
+        </tr>
+        
+        {data?.data?.data.map((item,index)=>{
+          return(
+          <tr key={index}>
+            <td>
+              <a href={`/docs/${item.attributes.Title}`}>
+                {item.attributes.Title}
+              </a>
+            </td>
+            <td>{item.attributes.Market || 'null'}</td>
+            <td>{item.attributes.Group || 'null'}</td>
+            <td>{item.attributes.Meeting || 'null'}</td>
+            <td>{item.attributes.Document_Type ||'null'}</td>
+            <td>{item.attributes.Meeting_Date ||'null'}</td>
 
-      <div className={s.result_parent}>
+          </tr>
+          )
+        })}
+
+        </tbody>
+      </table>
+
+       {/* <div className={s.result_parent}>
        {data?.data?.data.map((item,index) => {
             return (
-            
+                
                 <a  key={index} href={`/docs/${item.attributes.Title}`}>
                 <div  className={s.result_item}>
-                
+
                   <p>{item.attributes.Title}</p>
+                  <p>{item.attributes.Market}</p>
+                  <p>{item.attributes.Group}</p>
+                  <p>{item.attributes.Meeting}</p>
+                  <p>{item.attributes['Document_Type']}</p>
+
                 </div>
                 </a>
             )
             })}
-      </div>
+      </div> */}
     </>
   )
 }
